93edf5a03105e6adb25c3d522fe89aba49ac084c
Thanhmai1 <141890766+Thanhmai1@users.noreply.github.com>
2024-01-19 08:20:05 +0700

    Create Controller.php

---
+++ Controllers/Controller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class Controller extends BaseController
+{
+    // Traits provide additional functionality to the controller
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}
